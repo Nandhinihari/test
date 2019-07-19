f64edf6e7da22feeec4b944b670eb5562d917f7e
Nandhinihari <nandhinivkr93@gmail.com>
2019-07-19 14:48:45 +0530

    test

---
+++ src/Even.java
@@ -0,0 +1,12 @@
+
+public class Even {
+	public static void main(String[] args) {
+		int count = 0;
+		for (int i = 1; i <= 600; i++) {
+			count = count + i;
+		}
+		System.out.println(count);
+
+	}
+}
+	
